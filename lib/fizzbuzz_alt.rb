2875cf8e306cdc2b2e00aa3420232678786ee0c2
mbrad26 <mbrad26@gmail.com>
2020-04-06 12:09:49 +0000

    refactored for fizz test

---
+++ lib/fizzbuzz_alt.rb
@@ -0,0 +1,9 @@
+class Integer
+  
+  def fizzbuzz
+    if self % 3 == 0
+      'fizz'
+    end
+  end
+  
+end
--- spec/fizzbuzz_alt_spec.rb
+++ spec/fizzbuzz_alt_spec.rb
@@ -1,3 +1,5 @@
+require 'fizzbuzz_alt'
+
 describe 'fizzbuzz' do 
 	it 'return "fizz" when passed 3' do
 		expect(3.fizzbuzz).to eq 'fizz'
